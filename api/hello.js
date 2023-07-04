45ffbfffa5be75e1b63d86e8e31b28273390434f
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2023-07-05 01:40:00 +0900

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,11 +1,11 @@
 import { env } from "process";
-import { setGlobalDispatcher, ProxyAgent } from "undici";
+import { setGlobalDispatcher, ProxyAgent, Agent } from "undici";
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
 export default async (req, res) => {
   const { host, url = 'https://httpbin.org/get' } = req.query
-  setGlobalDispatcher(host ? new ProxyAgent(`http://${host}/`) : undefined)
+  setGlobalDispatcher(host ? new ProxyAgent(`http://${host}/`) : new Agent())
   const r = await fetch(url)
   res.status(200).send(await r.text())
 }
