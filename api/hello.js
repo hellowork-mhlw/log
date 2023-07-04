a55750cf356b61c7d275cc5aae9ebc84ae65811a
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2023-07-05 01:03:21 +0900

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,15 @@
 import { env } from "process";
 import { setGlobalDispatcher, ProxyAgent } from "undici";
 
+const https_proxy = 'http://35.213.91.45:80'
+
+if (https_proxy) {
+  // Corporate proxy uses CA not in undici's certificate store
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+  const dispatcher = new ProxyAgent({uri: new URL(https_proxy).toString() });
+  setGlobalDispatcher(dispatcher);
+}
+
 export default async (req, res) => {
   const { name = 'World' } = req.query
   const r = await fetch('https://httpbin.org/get')
