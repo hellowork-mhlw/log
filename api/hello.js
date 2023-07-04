e1431cbffbd53b3c49024f4477694ae5ff0e7585
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2023-07-05 01:38:52 +0900

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -5,7 +5,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
 export default async (req, res) => {
   const { host, url = 'https://httpbin.org/get' } = req.query
-  setGlobalDispatcher(new ProxyAgent(host ? `http://${host}/` : ''))
+  setGlobalDispatcher(host ? new ProxyAgent(`http://${host}/`) : undefined)
   const r = await fetch(url)
   res.status(200).send(await r.text())
 }
