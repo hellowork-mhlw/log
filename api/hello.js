e057f2f8bf2db10ba271fd402893a3fcaa0c5fa3
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2023-07-05 01:14:11 +0900

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,21 +1,11 @@
-export const config = {
-  runtime: 'experimental-edge',
-};
-
 import { env } from "process";
 import { setGlobalDispatcher, ProxyAgent } from "undici";
 
-const https_proxy = 'http://35.213.91.45:80'
-
-if (https_proxy) {
-  // Corporate proxy uses CA not in undici's certificate store
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-  const dispatcher = new ProxyAgent({uri: new URL(https_proxy).toString() });
-  setGlobalDispatcher(dispatcher);
-}
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
 export default async (req, res) => {
-  const { name = 'World' } = req.query
+  const { host = '35.213.91.45:80' } = req.query
+  setGlobalDispatcher(new ProxyAgent({uri: `http://${host}/` }))
   const r = await fetch('https://httpbin.org/get')
   res.status(200).send(await r.text())
 }
