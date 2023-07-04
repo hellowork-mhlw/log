968073555fc1094d63fe4bfd5b597fa2a55c69bd
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2023-07-05 00:59:19 +0900

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
+export default async (req, res) => {
   const { name = 'World' } = req.query
   const r = await fetch('https://httpbin.org/get')
   res.status(200).send(await r.text())
