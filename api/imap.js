33bee5d359730606d8520209e72010b8f31b3a25
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2024-11-22 00:14:37 +0900

    Update imap.js

--- api/imap.js
+++ api/imap.js
@@ -1,73 +1,69 @@
-import { simpleParser } from 'mailparser';
-import Imap from 'imap';
+import { simpleParser } from 'mailparser'
+import Imap from 'imap'
 
-async function handler() {
-  console.log(process.env.IMAP_HOST)
-  const imapConfig = {
-    user: process.env.IMAP_USER,
-    password: process.env.IMAP_PASSWORD,
-    host: process.env.IMAP_HOST,
-    port: 993,
-    tls: true,
-  };
-
-  const imap = new Imap(imapConfig);
-
-  imap.once('ready', () => {
-    imap.openBox('INBOX', true, (err, box) => {
-      if (err) {
-        return;
-      }
-
-      const fetch = imap.seq.fetch('1:10', {
-        bodies: '',
-        struct: true,
-      });
-
-      const emails = [];
-
-      fetch.on('message', (msg, seqno) => {
-        msg.on('body', (stream, info) => {
-          simpleParser(stream, (err, parsed) => {
-            if (err) {
-              console.error('Error parsing email:', err);
-              return;
-            }
-            emails.push({
-              subject: parsed.subject,
-              from: parsed.from.text,
-              date: parsed.date,
-              text: parsed.text,
-            });
-            console.log(emails)
-          });
-        });
-      });
-
-      fetch.once('end', () => {
-        imap.end();
-      });
-    });
-  });
-
-  imap.once('error', (err) => {
-    console.error('IMAP error:', err);
-  });
-
-  imap.connect();
+async function handler(imapConfig) {
+  return new Promise((resolve, reject) => {
+    const imap = new Imap(imapConfig)
+    imap.once('ready', () => {
+      imap.openBox('INBOX', true, (err, box) => {
+        if (err) {
+          return
+        }
+  
+        const fetch = imap.seq.fetch('1:10', {
+          bodies: '',
+          struct: true,
+        })
+  
+        const emails = []
+  
+        fetch.on('message', (msg, seqno) => {
+          msg.on('body', (stream, info) => {
+            simpleParser(stream, (err, parsed) => {
+              if (err) {
+                console.error('Error parsing email:', err)
+                return
+              }
+              emails.push({
+                subject: parsed.subject,
+                from: parsed.from.text,
+                date: parsed.date,
+                text: parsed.text,
+              })
+            })
+          })
+        })
+  
+        fetch.once('end', () => {
+          imap.end()
+          resolve(emails)
+        })
+      })
+    })
+  
+    imap.once('error', (err) => {
+      console.error('IMAP error:', err)
+    })
+  
+    imap.connect()
+  })
 }
 
 
 export async function GET(request) {
-  console.log(request)
-  handler()
-
-  const data = { message: `Hello2 from ${process.env.VERCEL_REGION}` };
+  const url = new URL(request.url)
+  const data = await handler({
+    user: url.searchParams.get('user'),
+    password: url.searchParams.get('password'),
+    host: url.searchParams.get('host'),
+    port: 993,
+    tls: true,
+  })
 
   const headers = {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*"
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
   }
 
-  return new Response(JSON.stringify(data), { headers });
+  return new Response(JSON.stringify(data), { headers })
 }
