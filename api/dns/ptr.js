93c3e77ae888f122ccee14806cbf842d713b53ed
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2024-11-23 02:27:52 +0000

    a

--- api/dns/ptr.js
+++ api/dns/ptr.js
@@ -2,8 +2,7 @@ import { resolve4, reverse } from 'node:dns/promises'
 
 export async function GET(request) {
     try {
-        console.log(request)
-        const ips = await resolve4("example.com")
+        const ips = await resolve4(request.url.searchParams.get('hostname'))
         if (ips.length) {
             const hostnames = await reverse(ips[0])
             return Response.json(hostnames)
