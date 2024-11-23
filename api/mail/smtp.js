8fc57382088e23edd0798eea6536fa4376d91ad1
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2024-11-23 01:12:29 +0000

    a

--- api/mail/smtp.js
+++ api/mail/smtp.js
@@ -7,33 +7,9 @@ export async function POST(request) {
     }
 
     try {
-        // リクエストボディを解析
-        const { host, port, secure, user, pass, to, subject, text, html } = await request.json()
-
-        // Nodemailerトランスポート設定
-        const transporter = nodemailer.createTransport({
-            host,
-            port,
-            secure, // true for 465, false for other ports
-            auth: {
-                user, // SMTPユーザー名
-                pass, // SMTPパスワード
-            },
-        })
-
-        // メールの送信設定
-        const mailOptions = {
-            from: user, // 送信元アドレス
-            to, // 宛先アドレス（カンマ区切りで複数指定可能）
-            subject, // 件名
-            text, // テキスト形式の本文
-            html, // HTML形式の本文（任意）
-        }
-
-        // メール送信
-        const info = await transporter.sendMail(mailOptions)
-
-        return new Response(JSON.stringify({ message: 'Email sent', info }), { headers })
+        const { transport, mail } = await request.json()
+        const info = await nodemailer.createTransport(transport).sendMail(mail)
+        return new Response(JSON.stringify(info), { headers })
     } catch (error) {
         return new Response(error.message, { status: 400, headers })
     }
