ff4d2e90cfd0d7b958cf6e0fa6b37ed642e34c59
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2024-11-23 04:37:55 +0000

    add

---
+++ api/ftp/list.js
@@ -0,0 +1,18 @@
+import { Client } from 'basic-ftp'
+
+export async function POST(request) {
+    const client = new Client()
+    client.ftp.verbose = true
+    try {
+        const { options } = await request.json()
+        await client.access(options)
+        const info = await client.list()
+        return Response.json(info)
+    } catch (error) {
+        return new Response.json(error.message, { status: 400 })
+    }
+}
+
+export async function OPTIONS() {
+    return new Response()
+}
