4679dc141cc95008f0415a0c3fcb2e3ce170dac4
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2024-11-25 03:24:43 +0000

    a

---
+++ api/ftp/download.js
@@ -0,0 +1,20 @@
+import { Client } from 'basic-ftp'
+
+export async function POST(request) {
+    const client = new Client()
+    client.ftp.verbose = true
+    try {
+        const options = Object.fromEntries(new URL(request.url).searchParams)
+        await client.access(options)
+        await client.downloadTo(options.filename, options.filename)
+        client.close()
+        return Response.json(info)
+    } catch (error) {
+        client.close()
+        return Response.json(error.message, { status: 400 })
+    }
+}
+
+export async function OPTIONS() {
+    return new Response()
+}
