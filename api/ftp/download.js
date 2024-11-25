0bf469543fea015321220a2907ea569107d1915f
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2024-11-25 04:17:09 +0000

    a

--- api/ftp/download.js
+++ api/ftp/download.js
@@ -1,15 +1,17 @@
 import { Client } from 'basic-ftp'
-import { Readable, Writable } from 'node:stream'
+import { Readable } from 'node:stream'
 
 export async function GET(request) {
     const client = new Client()
     try {
         const options = Object.fromEntries(new URL(request.url).searchParams)
         await client.access(options)
-        const writableStream = new Writable()
-        await client.downloadTo(writableStream, options.filename)
+        const passThroughStream = new Readable({
+            read() {} // No-op read, data will be pushed from FTP download
+        })
+        await client.downloadTo(passThroughStream, options.filename)
         client.close()
-        return new Response(Readable.from(writableStream), {
+        return new Response(passThroughStream, {
             headers: {
                 'Content-Type': 'application/octet-stream',
                 'Content-Disposition': `attachment; filename="${options.filename}"`,
