9e6414514214bc0dc30afffeb13a72880d68a570
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2024-11-25 03:08:16 +0000

    a

--- api/ftp/upload.js
+++ api/ftp/upload.js
@@ -4,14 +4,18 @@ import { Readable } from 'node:stream'
 export async function POST(request) {
     const formData = await request.formData()
     const files = formData.get('files')
-    console.log(files)
+    console.log(files, files.length)
+    for (const file of files) {
+        console.log(file)
+        console.log(file.name)
+    }
     const client = new Client()
     client.ftp.verbose = true
     try {
         const options = Object.fromEntries(new URL(request.url).searchParams)
         await client.access(options)
         let message = ''
-        for (const file of files.files) {
+        for (const file of files) {
             console.log(file)
             const stream = Readable.from(file.stream())
             const info = await client.uploadFrom(stream, file.name)
