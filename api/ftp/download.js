0c9afa1a63dce3d7e0ce2d53be989d4e389d6361
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2024-11-25 03:46:41 +0000

    add

--- api/ftp/download.js
+++ api/ftp/download.js
@@ -1,15 +1,15 @@
 import { Client } from 'basic-ftp'
-import { Writable } from 'node:stream'
+import { Readable, Writable } from 'node:stream'
 
 export async function GET(request) {
     const client = new Client()
     try {
         const options = Object.fromEntries(new URL(request.url).searchParams)
         await client.access(options)
-        const writeStream = new Writable()
-        await client.downloadTo(writeStream, options.filename)
+        const writableStream = new Writable()
+        await client.downloadTo(writableStream, options.filename)
         client.close()
-        return new Response(writeStream, {
+        return new Response(Readable.from(writableStream), {
             headers: {
                 'Content-Type': 'application/octet-stream',
                 'Content-Disposition': `attachment; filename="${options.filename}"`,
