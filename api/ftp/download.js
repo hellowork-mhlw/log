23d0b8e5151aea67c3d7851c4baeb1480d1d3e56
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2024-11-25 04:27:13 +0000

    a

--- api/ftp/download.js
+++ api/ftp/download.js
@@ -6,9 +6,13 @@ export async function GET(request) {
     try {
         const options = Object.fromEntries(new URL(request.url).searchParams)
         await client.access(options)
+        console.log(options.filename, new Date(), 0)
         const passThroughStream = new PassThrough()
+        console.log(options.filename, new Date(), 1)
         await client.downloadTo(passThroughStream, options.filename)
+        console.log(options.filename, new Date(), 2)
         client.close()
+        console.log(options.filename, new Date(), 3)
         return new Response(passThroughStream, {
             headers: {
                 'Content-Type': 'application/octet-stream',
