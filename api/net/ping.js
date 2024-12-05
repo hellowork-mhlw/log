9c81833174860fb6166e31d72b55bd1458945a70
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2024-12-05 13:38:21 +0000

    add

--- api/net/ping.js
+++ api/net/ping.js
@@ -1,9 +1,23 @@
-import ping from 'ping'
+import ping from 'net-ping'
+
+async function pingHost(target) {
+    const session = ping.createSession();
+
+    return new Promise((resolve, reject) => {
+        session.pingHost(target, (error, target) => {
+            if (error) {
+                reject(`${target}: ${error.toString()}`);
+            } else {
+                resolve(`${target}: Alive`);
+            }
+        });
+    });
+}
 
 export async function GET(request) {
     try {
         const host = new URL(request.url).searchParams.get('host') ?? 'google.com'
-        return Response.json(await ping.promise.probe(host))
+        return Response.json(await pingHost(host))
     } catch (error) {
         return Response.json(error.message, { status: 400 })
     }
